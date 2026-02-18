37ebcfeb217c7a115312fdc5467c7269c14e563b
Michal Černý <cerny.michal93@gmail.com>
2026-02-18 13:09:49 +0100

    update

--- server/src/main/java/cz/timetracker/ApplicationRun.java
+++ server/src/main/java/cz/timetracker/ApplicationRun.java
@@ -1,11 +1,14 @@
 package cz.timetracker;
 
+import cz.timetracker.configuration.JWTService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableConfigurationProperties(JWTService.class)
 public class ApplicationRun {
 
     public static void main(String[] args) {
